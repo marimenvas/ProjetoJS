8dde9b944d402b1093a099f92be0703ab037c937
Marianela Méndez <67439634+marimenvas@users.noreply.github.com>
2020-08-04 01:08:48 -0300

    second commit

--- JavaScript.js
+++ JavaScript.js
@@ -63,8 +63,21 @@ var formulario = document.getElementById("formulario"),
     }
 
     formulario.addEventListener("submit", validarFormulario);
-   
-   
 
+    var contenido = document.querySelector('#contenido')
+	fetch('https://api.github.com/users/marimenvas/repos')
+		.then(function(response){
+			return response.json()
+	})
 
-
+		.then(function(data){
+			console.log(data.length)
+			 var cont = 0
+			 for(cont=0;cont<data.length;cont++){ 
+ 		    	 
+				
+			 contenido.innerHTML +=` <div class="card">
+            <a href="${data[cont].html_url}" target="blank_">  ${data[cont].name} </a>
+            </div>`
+        }
+    })	
